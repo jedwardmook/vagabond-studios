66830ca3b80db9657925dd167fda5e5b180884c4
JedwardMook <jedwardmook@yahoo.com>
2024-04-18 14:58:57 -0500

    adds first schema and creates logic to hide nav, footer, header from admin

--- src/Components/Header.tsx
+++ src/Components/Header.tsx
@@ -1,8 +1,13 @@
+'use client';
 import Image from "next/image";
 import styles from "./header.module.css";
+import useIsAdminRoute from "../app/utils/routeUtils";
 
 export default function Header() {
+	const isAdminRoute = useIsAdminRoute();
+
 	return (
+		!isAdminRoute &&
 		<div className={styles.main}>
 			<a href="/">
 				<Image

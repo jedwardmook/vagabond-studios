e3bdbb7a17793d6e92db352cb4cd4e08344253de
JedwardMook <jedwardmook@yahoo.com>
2024-04-23 15:02:34 -0500

    style: sets up projectsContext and reformats

---
+++ src/app/context/projectsContext.tsx
@@ -0,0 +1,65 @@
+'use client';
+import {createContext, useState, useEffect } from 'react';
+import { createClient, groq } from 'next-sanity';
+import { projectId, dataset, apiVersion } from '../../../sanity/env';
+
+type Image = {
+    url: string
+  };
+
+type Project = {
+    title: string,
+    year: string,
+    images: Image[],
+    description: string,
+  };
+
+type ProjectsContextProps = {
+    projects: Project[];
+    fetchProjects: () => void;
+  };
+
+const ProjectsContext = createContext<ProjectsContextProps>({
+    projects: [],
+    fetchProjects: () => {},
+});
+
+export const ProjectsProvider = ({ children } : {children: React.ReactNode}) => {
+    const [projects, setProjects] =  useState([]);
+
+    const client = createClient({
+        projectId,
+        dataset,
+        apiVersion,
+    });
+
+    const fetchProjects = async () => {
+        const query = groq`*[_type == 'project']{
+			_id,
+			_createdAt,
+			title,
+			artist,
+			year,
+			mediums,
+			images[] {
+				"url": asset->url
+			},
+			description
+		}`;
+
+        const data = await client.fetch(query);
+        setProjects(data);
+    };
+
+    useEffect(() => {
+        fetchProjects();
+    });
+
+    return (
+        <ProjectsContext.Provider value={{projects, fetchProjects}}>
+            {children}
+        </ProjectsContext.Provider>
+    );
+};
+
+export default ProjectsContext;
